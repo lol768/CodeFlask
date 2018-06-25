afb29352a0a7d4ae424f43bcadd3e2910ef97b62
Kyle Clemens <git@kyleclemens.com>
2018-06-25 08:50:15 -0400

    sometimes your editor needs to get a little... sassy

--- rollup.config.js
+++ rollup.config.js
@@ -6,19 +6,19 @@ import uglify from 'rollup-plugin-uglify';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/codeflask.js',
+	input: 'src/codesass.js',
 	output: [
 		{
 			sourcemap: false,
 			format: 'umd',
-			name: 'CodeFlask',
-			file: 'build/codeflask.min.js'
+			name: 'CodeSass',
+			file: 'build/codesass.min.js'
 		},
 		{
 			sourcemap: false,
 			format: 'es',
-			name: 'CodeFlask',
-			file: 'build/codeflask.module.js'
+			name: 'CodeSass',
+			file: 'build/codesass.module.js'
 		},
 	],
 	plugins: [
